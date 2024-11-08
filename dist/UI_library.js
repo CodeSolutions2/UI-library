26fab32b15a0ab22a4a2ee7adb02a6b3a6c28ba6
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-11-08 18:24:10 +0100

    Update UI_library.js

--- dist/UI_library.js
+++ dist/UI_library.js
@@ -5,28 +5,49 @@ export async function UI_library(obj) {
 	function run_selections(obj) {
 
 		var ui_num = Object.keys(obj.ui_elements);
+
+		var htmlString = "";
 		
-		// for (let i=0; i<ui_num.length; i++) { 
+		for (let i=0; i<ui_num.length; i++) { 
+
+			var key_name = ui_num.at(i);
+			var value_name = obj.ui_elements[key_name];
 
 			// Detect the type of selected ui to use the correct function.
-			// if (ui_num.at(i) == ) {
+			if ((/dropdown/g).test(ui_num.at(i)) == true) {
+				htmlString = htmlString + dropdown_construct_DOM_elements_w_strings();
+				
+			} else if ((/input/g).test(ui_num.at(i)) == true) {
+				htmlString = htmlString + input_construct_DOM_elements_w_strings();
+				
+			} else if ((/navbar/g).test(ui_num.at(i)) == true) {
+				htmlString = htmlString + navbar_construct_DOM_elements_w_strings();
+				
+			} else if ((/radio/g).test(ui_num.at(i)) == true) {
+				htmlString = htmlString + radio_construct_DOM_elements_w_strings();
+				
+			} else if ((/sidebar/g).test(ui_num.at(i)) == true) {
+				htmlString = htmlString + sidebar_construct_DOM_elements_w_strings();
+				
+			} else if ((/tab/g).test(ui_num.at(i)) == true) {
+				htmlString = htmlString + tab_construct_DOM_elements_w_strings(key_name, value_name);
 
-			// }
+			} else if ((/toggle/g).test(ui_num.at(i)) == true) {
+				htmlString = htmlString + toggle_construct_DOM_elements_w_strings();
+			}
 
-		// }
+		}
 
-		// const htmlString_IndexElements = '<!DOCTYPE html><html><head></head><body>'+htmlString+'<link href="./dist/ui_styles.css" rel="stylesheet" integrity="" crossorigin="anonymous"></body></html>';
-
-		return ui_num;
+		return htmlString;
 	}
 
 	// -----------------------------------------
 	
-	function tabs_construct_DOM_elements_w_strings(tab_num) {
+	function tab_construct_DOM_elements_w_strings(key_name, value_name) {
 
 		// Create tabs
 		var aElement = "";
-		for (let i=0; i<tab_num; i++) {
+		for (let i=0; i<value_name; i++) {
 			var aElement_class = i == 0 ? "tabs-nav-link active" : "tabs-nav-link";
 			aElement = aElement + `<a class="${aElement_class}" href="#" id="tabs_${i}">Tab ${i}</a>`;
 		}
@@ -35,13 +56,13 @@ export async function UI_library(obj) {
 		var divElement = "";
 		var h1Element = "";
 		var pElement = "";
-		for (let i=0; i<tab_num; i++) {
+		for (let i=0; i<value_name; i++) {
 			h1Element = `<h1 class="content-title" id="tabs_content_title_${i}">Title ${i}</h1>`;
 			pElement = `<p class="content-text" id="tabs_content_text_${i}">Text ${i}</p>`;
 			divElement = divElement + `<div class="content-container" id="tabs_content_${i}">` + h1Element + pElement + "</div>";
 		}
 		
-		 return `<div class="tabs ${tabs_container_class}" id="tabs_container">
+		 return `<div class="tabs ${key_name}" id="tabs_container">
 			  <nav class="tabs-nav">
 			    ${aElement}
 			  </nav>
